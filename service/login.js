43f8ddb3e4669fc93085f4f1a295045d7b3fd2a2
malex <mircea.alexandru@gmail.com>
2016-10-17 15:52:34 +0300

    support for token

--- service/login.js
+++ service/login.js
@@ -19,18 +19,21 @@ class Login {
 		const user = request.pre.user;
 
 		let data = _.merge({}, user);
-		request.server.plugins['utils-mail'].send({
-			template: 'forgot-password',
-			data: data,
-			subject: 'Reset password',
-			to: user.email
-		}, function (err) {
-			if (err) {
-				return reply(Boom.badRequest('Invalid email or key.'));
-			}
+		request.server.plugins['utils-token'].save({userId: user.id}, (err, token) => {
+			data.token = token
+			request.server.plugins['utils-mail'].send({
+				template: 'forgot-password',
+				data: data,
+				subject: 'Reset password',
+				to: user.email
+			}, function (err) {
+				if (err) {
+					return reply(Boom.badRequest('Invalid email or key.'));
+				}
 
-			reply({})
-		});
+				reply({})
+			});
+		})
 	}
 
 	resetPassword(request, reply) {
